4f87f3715caa37716b8c91c49f25a546eb34d7f1
Cody Williams <c_m_williams@outlook.com>
2022-07-12 16:05:49 -0700

    integrate database in routes

--- index.js
+++ index.js
@@ -1,108 +1,94 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
+// IMPORT MONGODB MODEL(S)
+const ContactModel = require('./models/note');
+
+// EXPRESS MIDDLEWARE
 const cors = require('cors');
 const morgan = require('morgan');
 morgan.token('body', function(req, res) { return JSON.stringify(req.body) });
 
-app.use(cors());
-app.use(express.static('build'));
-app.use(express.json());
-app.use(morgan(':method :url HTTP/:http-version :body'));
+app.use(cors()); // enable CORS
+app.use(express.static('build')); // serve static files from build directory if request matches one of its files
+app.use(express.json()); // parse HTTP request bodies into JSON, attach to body property on request objects
+app.use(morgan(':method :url HTTP/:http-version :body')); // log all http request info to std out
 
-let contacts = [
-  {
-    "firstName": "Cody",
-    "lastName": "Williams",
-    "phoneNumber": "(818) 795-9614",
-    "id": 1
-  },
-  {
-    "firstName": "Corey",
-    "lastName": "Ly",
-    "phoneNumber": "(800) 800-0000",
-    "id": 2
-  },
-  {
-    "firstName": "Sidney",
-    "lastName": "Clark",
-    "phoneNumber": "(740) 420-6969",
-    "id": 3
-  }
-];
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello, world</h1>')
+// GET ALL CONTACTS
+app.get('/api/contacts', (req, res, next) => {
+  ContactModel.find({})
+              .then(contacts => res.json(contacts))
+              .catch(err => next(err));
 });
 
-app.get('/api/contacts', (req, res) => {
-  res.json(contacts);
-});
-
-app.get('/info', (req, res) => {
-  const contactCount = contacts.length;
-  const currentDateTime = new Date().toString()
-  res.send(`<h3>Phonebook has info for ${contactCount} people</h3>\n
-            <h4>${currentDateTime}</h4>`);
-})
-
-app.get('/api/contacts/:id', (req, res) => {
+// GET A SINGLE CONTACT
+app.get('/api/contacts/:id', (req, res, next) => {
   const id = req.params.id;
-  const contact = contacts.find(c => c.id === id);
-  if (contact) {
-    res.json(contact);
-  } else {
-    res.status(404).end()
-  }
+  ContactModel.findById(id)
+              .then(contact => {
+                if (contact) {
+                  res.json(contact);
+                } else {
+                  res.status(404).end()
+                }
+              })
+              .catch(err => next(err));
 });
 
-app.put('/api/contacts/:id', (req, res) => {
+// UPDATE A SINGLE CONTACT
+app.put('/api/contacts/:id', (req, res, next) => {
   const id = Number(req.params.id);
-  const contact = req.body;
-  contact.id = id;
-  contacts = contacts.map(c => c.id === id ? contact : c);
-  res.send(contact);
+  const updatedContact = req.body;
+  ContactModel.findByIdAndUpdate(id, updatedContact, { new: true, runValidators: true, context: 'query' }) // options object specifies that returned value should be the updated note instead of old note, and that schema validators should be run
+              .then(result => res.json(result))
+              .catch(err => next(err));
 });
 
-app.delete('/api/contacts/:id', (req, res) => {
+// DELETE A SINGLE CONTACT
+app.delete('/api/contacts/:id', (req, res, next) => {
   const id = req.params.id;
-  contacts = contacts.filter(c => c.id !== id);
-  res.status(204).end();
+  ContactModel.findByIdAndDelete(id)
+              .then(result => {
+                if (result) {
+                  res.status(200).end();
+                } else {
+                  res.status(204).end();
+                }
+              })
+              .catch(err => next(err));
 });
 
-const nextId = () => contacts.length > 0 ? Math.max(...contacts.map(c => c.id)) + 1 : 1;
-
-const missingNameAndNumber = (contact) => {
-  return (!contact.firstName && !contact.phoneNumber) &&
-         (contact.firstName.trim() !== '' && contact.phoneNumber.trim() !== '');
-};
-
-const nameAlreadyExists = (contact) => contacts.some(c => {
-  return c.firstName.toLowerCase().trim() === contact.firstName.toLowerCase().trim() && 
-         c.lastName.toLowerCase().trim() === contact.lastName.toLowerCase().trim();
-});
-
-app.post('/api/contacts', (req, res) => {
-  console.log(req.body);
+// CREATE A SINGLE CONTACT
+app.post('/api/contacts', (req, res, next) => {
   const contact = req.body;
-
-  if (missingNameAndNumber(contact)) {
-    return res.status(400).json({ error: "first name and/or phone number missing" });
-  } else if (nameAlreadyExists(contact)) {
-    return res.status(400).json({ error: `${contact.firstName} ${contact.lastName} already exists.`})
-  }
-
-  contact.id = nextId();
-  contacts = contacts.concat(contact);
-  
-  res.json(contact);
+  const newContact = new ContactModel(contact);
+  newContact.save()
+            .then(result => {
+              res.json(result);
+              mongoose.connection.close();
+            })
+            .catch(err => next(err));
 });
 
-const unknownRoute = (req, res) => {
-  res.status(404).send({ error: 'unknown route' })
-}
-
+// MIDDLEWARE: HANDLE AN UNKNOWN ENDPOINT / ROUTE
+const unknownRoute = (req, res) => res.status(404).send({ error: 'unknown route' });
 app.use(unknownRoute);
 
-const PORT = process.env.PORT || 3001;
+// MIDDLEWARE: ERROR HANDLER
+const errorHandler = (err, req, res, next) => {
+  console.error(err.message);
+
+  if (err.name === 'CastError') {
+    return res.status(400).send({ err: 'Malformatted Mongo ID' });
+  } else if (err.name === 'ValidationError') {
+    return res.status(400).json({ err: err.message });
+  }
+
+  next(err);
+}
+app.use(errorHandler);
+
+// START WEB SERVER
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Web server running on port ${PORT}`));
---
+++ models/note.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const DB_URI = process.env.MONGO_DB_URI;
+
+console.log('Connecting to MongoDB...');
+mongoose.connect(DB_URI)
+        .then(res => console.log('Connected to MongoDB'))
+        .catch(err => console.log('Error connecting to MongoDB:', err.message));
+
+const contactSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    minLength: 1,
+    required: true
+  },
+  lastName: String,
+  phoneNumber: {
+    type: String,
+    minLength: 1,
+    required: true
+  }
+});
+
+contactSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+})
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = Contact;
