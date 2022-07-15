5f96996ac98d106362b6de59893d457157a62150
Cody Williams <c_m_williams@outlook.com>
2022-07-14 20:03:53 -0700

    refactoring to put state into respective components

--- build/precache-manifest.98e4e0c308cfae7f172d758462408007.js
+++ build/precache-manifest.98e4e0c308cfae7f172d758462408007.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "aa41a8e2974f8045a63c",
+    "revision": "182bb8bda754c0d1b66a",
     "url": "/static/css/main.5b8f69c2.chunk.css"
   },
   {
-    "revision": "aa41a8e2974f8045a63c",
-    "url": "/static/js/main.aa41a8e2.chunk.js"
+    "revision": "182bb8bda754c0d1b66a",
+    "url": "/static/js/main.182bb8bd.chunk.js"
   },
   {
     "revision": "269d177dfa7b1bf5e155",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.7ac45543.js"
   },
   {
-    "revision": "2fcae7c1e70a897a51975d0521d96b4a",
+    "revision": "01ba1c4397797c0a1b62717eba6b1a51",
     "url": "/index.html"
   }
 ];
